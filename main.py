952882093aa9b31debb7ff646dcc1e05033c0930
DevHyung <khuphj@gmail.com>
2018-07-10 21:16:44 +0900

    Add main File

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import datetime
 import xlsxwriter
 # Create a workbook and add a worksheet.
-fileName = input("파일명을 입력하세요 ::")
+fileName = input("파일명을 입력하세요(확장자포함) ::")
 workbook = xlsxwriter.Workbook(fileName+'_RESULT.xlsx')
 worksheet = workbook.add_worksheet()
-worksheet.write_row(0,0,['카드번호','트랜잭션ID','사용자구분코드','출발ID','출발_호선','출발수단','도착ID','도착_호선','도착수단','환승시간(초)'])
+worksheet.write_row(0,0,['카드번호','트랜잭션ID','사용자구분코드','출발ID','출발_호선','출발수단','출발시간','도착ID','도착_호선','도착수단','도착시간','환승시간(초)'])
 row = 1
 col = 0
 
@@ -19,6 +19,8 @@ def get_second(first, last):
         return "안찍고내림"
 
 f = open(fileName,'r',encoding='utf8').readlines()
+f2 = open(fileName+'_RESULT.csv','w')
+f2.writelines(['카드번호',',','트랜잭션ID',',','사용자구분코드',',','출발ID',',','출발_호선',',','출발수단',',','출발시간',',','도착ID',',','도착_호선',',','도착수단',',','도착시간',',','환승시간(초)',',','\n'])
 #번호,일련번호,카드번호,승차일시,트랜잭션ID,교통수단코드,환승횟수,표준노선ID,교통사업자ID,표준차량ID,사용자구분코드,운행출발일시,표준승차정류장ID,하차일시,표준하차정류장ID,이용객수_다인승,승차금액,하차금액,임시필드
 lines = f
 oldCardNo = ''
@@ -52,30 +54,29 @@ for line in lines[1:]:
 
             #출발수단코드 계산
             if len(str(lines[firstIdx].split(',')[14]).strip()) <= 4: #지하철코드
-                startCode = 0
+                startCode = '0'
             else:#버스코드
-                startCode = 1
+                startCode = '1'
             #도착수단코드계산
             if len(str(lines[lateIdx].split(',')[12]).strip()) <= 4: #지하철코드
-                endCode = 0
+                endCode = '0'
             else:#버스코드
-                endCode = 1
+                endCode = '1'
             print(lines[0].split(','))
             print(lines[firstIdx].split(','))
             print(lines[lateIdx].split(','))
 
-            print("카드번호 : {} \n 트랜잭션ID : {} \n 사용자구분코드 : {} \n 출발ID : {} \n 출발호선 : {} \n 출발수단 : {} \n 도착 ID : {} \n 도착호선 : {} \n 도착수단 : {} \n 환승시간(초) : {}"
-                  .format( oldCardNo, oldTranjectionId, datas[10], lines[firstIdx].split(',')[14], lines[firstIdx].split(',')[14],
-                           startCode, lines[lateIdx].split(',')[12],lines[lateIdx].split(',')[12],endCode,get_second(lines[firstIdx].split(',')[13],lines[lateIdx].split(',')[3])))
-
-            a = [1, 2, 3, 4]
-            worksheet.write_row(row,col, [oldCardNo, oldTranjectionId, datas[10], lines[firstIdx].split(',')[14], lines[firstIdx].split(',')[14],startCode, lines[lateIdx].split(',')[12],lines[lateIdx].split(',')[12],endCode,get_second(lines[firstIdx].split(',')[13],lines[lateIdx].split(',')[3])])
+            #print("카드번호 : {} \n 트랜잭션ID : {} \n 사용자구분코드 : {} \n 출발ID : {} \n 출발호선 : {} \n 출발수단 : {} \n 도착 ID : {} \n 도착호선 : {} \n 도착수단 : {} \n 환승시간(초) : {}"
+            #      .format( oldCardNo, oldTranjectionId, datas[10], lines[firstIdx].split(',')[14], lines[firstIdx].split(',')[14],
+            #               startCode, lines[lateIdx].split(',')[12],lines[lateIdx].split(',')[12],endCode,get_second(lines[firstIdx].split(',')[13],lines[lateIdx].split(',')[3])))
+            worksheet.write_row(row,col, [oldCardNo, oldTranjectionId, datas[10], lines[firstIdx].split(',')[14], lines[firstIdx].split(',')[14],startCode, lines[firstIdx].split(',')[13],lines[lateIdx].split(',')[12],lines[lateIdx].split(',')[12],endCode,lines[lateIdx].split(',')[3],get_second(lines[firstIdx].split(',')[13],lines[lateIdx].split(',')[3])])
+            f2.writelines([oldCardNo, ',',oldTranjectionId,',',datas[10], ',',lines[firstIdx].split(',')[14], ',',lines[firstIdx].split(',')[14],',',startCode, ',',lines[firstIdx].split(',')[13],',',lines[lateIdx].split(',')[12],',',lines[lateIdx].split(',')[12],',',endCode, ',',lines[lateIdx].split(',')[3],',',str ( get_second(lines[firstIdx].split(',')[13],lines[lateIdx].split(',')[3]) ),',','\n'])
             row+=1
-
         oldCardNo = newCardNo
         oldTranjectionId = newTranjectionId
         oldLineIdx += 1
 workbook.close()
+f2.close()
 # 데이터셋 보는곳
 """
 for line in lines:
